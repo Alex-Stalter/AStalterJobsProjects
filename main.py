823d86f329d6385c03c2623019d0ac8208d18217
Alex <astalter18@gmail.com>
2021-02-02 14:56:01 -0500

    Added for loop into get_data function in order to get all of the data from the file.

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ def get_data(url:str):
     all_data = []
     full_url = f"{url}&api_key={secrets.api_key}"
     response = requests.get(full_url)
-    if response.status_code != 200:
-        print(response.text)
-        return[]
     json_data = response.json()
-    results = json_data['results']
-    all_data.extend(results)
+    metadata = json_data['metadata']
+    total_data = metadata['total']
+    per_page = metadata['per_page']
+    pages = round(total_data/per_page)
+
+    for x in range(pages):
+        response = requests.get(full_url)
+        if response.status_code != 200:
+            print(response.text)
+            return[]
+        json_data = response.json()
+        results = json_data['results']
+        all_data.extend(results)
+        full_url = f"{url}&api_key={secrets.api_key}&page={x+1}"
+
     return all_data
 
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,2018.student.size"
     all_data = get_data(url)
-    print(all_data)
+    for x in all_data:
+        print(x)
 
 
 if __name__ == '__main__':
