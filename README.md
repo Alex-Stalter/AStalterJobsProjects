0040b152fdf0078e24915aa8835607a48e51444c
Alex <astalter18@gmail.com>
2021-02-04 12:12:00 -0500

    Added write_to_file() function and created a file with the information from the query.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #Jobs Projects
-The following repository is for the COMP490 Senior Design and development. 
+The following repository is for the COMP490 Senior Design and development.
+
+TODO: add instructions on how to run code and steps that are needed to set up the proper environment.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import secrets
+import os
 
 
 def get_data(url: str):
@@ -25,13 +26,22 @@ def get_data(url: str):
     return all_data
 
 
+def write_to_file(data):
+    if os.path.exists("results.txt"):
+        os.remove("results.txt")
+    results_file = open("results.txt", 'x')
+    for x in data:
+        results_file.write(str(x))
+        results_file.write("\n")
+
+
 def main():
     url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&" \
           "fields=id,school.state,school.name,2018.student.size,2017.student.size," \
-          "2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
+          "2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall" \
+          "&sort=2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line"
     all_data = get_data(url)
-    for x in all_data:
-        print(x)
+    write_to_file(all_data)
 
 
 if __name__ == '__main__':
