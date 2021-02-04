4d6f1ece1d534ccddc95355254ad54112f3d16de
Alex <astalter18@gmail.com>
2021-02-03 20:38:28 -0500

    Added full query to the URL

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ def get_data(url:str):
 
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,2018.student.size"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,2018.student.size," \
+          "2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
     all_data = get_data(url)
     for x in all_data:
         print(x)
