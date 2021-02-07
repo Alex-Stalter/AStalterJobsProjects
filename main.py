8a6c871ca2b3e4ff6a559da2af89e3c62726aa84
Alex <astalter18@gmail.com>
2021-02-06 20:43:08 -0500

    Added format_url() function in order make it more readable and manageable, also clean_data which is still in progress

--- main.py
+++ main.py
@@ -8,11 +8,29 @@ import os
 # the function then loops through all of the pages given the information from the metadata.
 
 
+def format_url():
+    query = ["school.name", "school.state", "2018.student.size", "2017.student.size",
+             "2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line",
+             "2016.repayment.3_yr_repayment.overall"]
+    sort = query[4]
+    degree_type = "school.degrees_awarded.predominant=2,3"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?"+degree_type + "&fields=id"
+    for x in query:
+
+        url += "," + x
+    url += "&sort=" + sort
+
+    return url
+
+
 def get_data(url: str):
     # TODO: general clean up to make code more readable.
     all_data = []
     full_url = f"{url}&api_key={secrets.api_key}"
     response = requests.get(full_url)
+    if response.status_code != 200:
+        print(response.text)
+        return []
     json_data = response.json()
     metadata = json_data['metadata']
     total_data = metadata['total']
@@ -29,11 +47,29 @@ def get_data(url: str):
         all_data.extend(results)
         full_url = f"{url}&api_key={secrets.api_key}&page={x+1}"
 
-    return all_data
+    write_to_file(all_data)
+    # write_to_file(clean_data(all_data))
+
+
+def clean_data(unclean_data):
+    # TODO: Clean up results so that the file does not include unneeded things. Such as brackets and extra long labels.
+    cleaned_data = []
+    for x in unclean_data:
+        name = x['school.name']
+        state = x['school.state']
+        school_id = x['id']
+        a_size = x['2017.student.size']
+        b_size = x['2018.student.size']
+        earnings = x['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line']
+        repayment = x['2016.repayment.3_yr_repayment.overall']
+        cleaned_data.extend(f"school.name{name},school.state:{state},id:{school_id},2017.size:{a_size},"
+                            f"2018.size:{b_size},earning:{earnings},repayment:{repayment}")
+
+    return cleaned_data
 
 
 def write_to_file(data):
-    # TODO: Clean up results so that the file does not include unneeded things. Such as brackets and extra long labels.
+
     if os.path.exists("results.txt"):
         os.remove("results.txt")
     results_file = open("results.txt", 'x')
@@ -44,15 +80,9 @@ def write_to_file(data):
 
 
 def main():
-    # TODO: Clean up url so that it is easier to read and manage.
     # The URL query gets all of the minimum required data and sorts by earnings.
 
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&" \
-          "fields=id,school.state,school.name,2018.student.size,2017.student.size," \
-          "2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall" \
-          "&sort=2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line"
-    all_data = get_data(url)
-    write_to_file(all_data)
+    get_data(format_url())
 
 
 if __name__ == '__main__':
