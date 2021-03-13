e6e2f91f3e3ae737343470e0b9ae1c46e4e5a5ad
Alex <astalter18@gmail.com>
2021-03-13 17:54:43 -0500

    Fleshed out the GUI to added multiple views and buttons to accompany them.

--- jobs.py
+++ jobs.py
@@ -8,6 +8,8 @@ import openpyxl
 from typing import Tuple
 import jobsWindow
 import sys
+
+
 # import PySide6.QtWidgets
 
 
@@ -44,17 +46,20 @@ def format_url():
     url += "&sort=" + sort
 
     return url
+
+
 # Drops the table school so that no conflicts are created and a clean table can be worked in.
 # Once table has been dropped teh school table is created and all the columns created.
 
 
 def setup_db(cursor: sqlite3.Cursor):
     cursor.execute(''' DROP TABLE IF EXISTS school;''')
-    cursor.execute('''DROP TABLE if EXISTS jobs''')
+    cursor.execute('''DROP TABLE if EXISTS jobs;''')
+    cursor.execute('''DROP TABLE if EXISTS states;''')
     cursor.execute('''CREATE TABLE if NOT EXISTS school(
     school_id INTEGER PRIMARY KEY,
     name TEXT,
-    state TEXT,
+    state_abrev TEXT,
     size_2017 INTEGER,
     size_2018 INTEGER,
     earnings INTEGER,
@@ -62,8 +67,8 @@ def setup_db(cursor: sqlite3.Cursor):
     repayment_cohort FLOAT);''')
     cursor.execute('''CREATE TABLE if NOT EXISTS jobs(
     job_id INTEGER PRIMARY KEY,
-    state TEXT,
-    occupation_code TEXT,
+    state_name TEXT,
+    occupation_code INTEGER,
     title TEXT,
     employment INTEGER,
     salary_25th_percentile INTEGER);''')
@@ -74,18 +79,18 @@ def excel_import(excel_file: str):
     job_worksheet = excel.active
     occupation_groups = job_worksheet['J']
     rows_to_read = []
-    jobs_dict = []
+    excel_dict = []
     for group in occupation_groups:
         if group.value == "major":
             rows_to_read.append(group.row)
 
     for row in rows_to_read:
-        jobs_dict.append({'state': job_worksheet['B' + str(row)].value, 'code': job_worksheet['H' + str(row)].value,
-                          'title': job_worksheet['I' + str(row)].value,
-                          'employment': job_worksheet['K' + str(row)].value,
-                          'salary': job_worksheet['Y' + str(row)].value})
+        excel_dict.append({'state': job_worksheet['B' + str(row)].value, 'code': str(job_worksheet['H' + str(row)].value)[:2],
+                           'title': job_worksheet['I' + str(row)].value,
+                           'employment': job_worksheet['K' + str(row)].value,
+                           'salary': job_worksheet['Y' + str(row)].value})
     print("excel_import() has finished...sending data...")
-    return jobs_dict
+    return excel_dict
 
 
 # get_data takes the inputted URL and adds the api_key from secrets.py to the end of the url.
@@ -136,7 +141,7 @@ def insert_data(table_data, table: str, cursor: sqlite3.Cursor):
             earnings = data_element['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line']
             repayment_overall = data_element['2016.repayment.3_yr_repayment.overall']
             repayment_cohort = data_element['2016.repayment.repayment_cohort.3_year_declining_balance']
-            cursor.execute('''INSERT INTO SCHOOL (school_id, name, state, size_2017, size_2018, earnings,
+            cursor.execute('''INSERT INTO SCHOOL (school_id, name, state_abrev, size_2017, size_2018, earnings,
             repayment_overall, repayment_cohort) VALUES (?, ?, ?, ?, ?, ?, ?,?)''',
                            (school_id, name, state, size_2017, size_2018, earnings,
                             repayment_overall, repayment_cohort))
@@ -146,9 +151,14 @@ def insert_data(table_data, table: str, cursor: sqlite3.Cursor):
             occupation_title = data_element['title']
             employment = data_element['employment']
             salary = data_element['salary']
-            cursor.execute('''INSERT INTO JOBS (job_id, state, occupation_code,title, employment, salary_25th_percentile)
+            cursor.execute('''INSERT INTO JOBS (job_id, state_name, occupation_code,title, employment, salary_25th_percentile)
                         VALUES (?,?, ?, ?, ?, ?)''',
                            (None, state, occupation_id, occupation_title, employment, salary))
+        elif table == "states":
+            cursor.execute('''INSERT INTO STATES (state_id, state_name, state_arbev)
+                        VALUES(?,?,?)''', (None, data_element['state_name'], data_element['state.abrev']))
+
+
 # query_run makes running queries easier so that a cursor and string can be provided
 # and the results will be returned instead of rewriting execute statements everywhere.
 
@@ -166,14 +176,13 @@ def create_window():
 
 
 def main():
-
-    school_data = get_data(format_url())
-    jobs_data = excel_import("state_job_data.xlsx")
-    conn, cursor = open_db("jobs_db.sqlite")
-    setup_db(cursor)
-    insert_data(school_data, "school", cursor)
-    insert_data(jobs_data, "jobs", cursor)
-    close_db(conn)
+    #school_data = get_data(format_url())
+    #jobs_data = excel_import("state_job_data.xlsx")
+    #conn, cursor = open_db("jobs_db.sqlite")
+    #setup_db(cursor)
+    #insert_data(school_data, "school", cursor)
+    #insert_data(jobs_data, "jobs", cursor)
+    #close_db(conn)
     create_window()
 
 
--- jobsWindow.py
+++ jobsWindow.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import *
+from PySide6.QtWidgets import QPushButton, QLabel, QApplication, QListWidgetItem, QWidget, \
+    QTableWidgetItem, QTableWidget, QAbstractItemView, QLineEdit
 from typing import List, Dict
 import plotly.graph_objects as maps_plotly
 # import jobs
@@ -11,9 +12,13 @@ class JobsWindow(QWidget):
         self.update_button = QPushButton("Update Data", self)
         self.data_button = QPushButton("Run Data Visualization", self)
         self.back_button = QPushButton("Back", self)
+        self.text_visualization_button = QPushButton("Text Visualization", self)
+        self.map_visualization = QPushButton("Map Visualization", self)
         self.data_visualization_label = QLabel("Welcome to data visualization!", self)
         self.welcome_label = QLabel("Welcome to Jobs Data Visualization", self)
+        self.text_data_visualization = QTableWidget(self)
         self.us_map = maps_plotly.Figure(maps_plotly.Scattergeo())
+        self.update_box_01 = QLineEdit(self)
         self.list_control = None
         self.setup_window()
 
@@ -31,11 +36,28 @@ class JobsWindow(QWidget):
         self.back_button.move(25, 450)
         self.back_button.hide()
         self.data_visualization_label.hide()
+        self.data_visualization_label.move(20, 400)
+        self.text_visualization_button.move(400 - self.text_data_visualization.width(), 750)
+        self.text_visualization_button.clicked.connect(self.text_visualization)
+        self.text_visualization_button.hide()
+        self.map_visualization.move(self.text_visualization_button.x()+self.text_visualization_button.width(),
+                                    self.text_visualization_button.y())
+        self.map_visualization.clicked.connect(self.run_map_visualization)
+        self.map_visualization.hide()
         self.welcome_label.move(150, 150)
         self.us_map.update_geos(visible=False, resolution=110, scope="usa",
                                 showcountries=True, countrycolor="Black",
                                 showsubunits=True, subunitcolor="Grey")
         self.us_map.update_layout()
+        self.text_data_visualization.setRowCount(5)
+        self.text_data_visualization.setColumnCount(5)
+        self.text_data_visualization.hide()
+        self.text_data_visualization.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.text_data_visualization.resize(self.text_data_visualization.size().width() *
+                                            self.text_data_visualization.columnCount() + 25, 300)
+        self.text_data_visualization.setItem(0, 1, QTableWidgetItem("Data Entered"))
+        self.update_box_01.move(230, 240)
+        self.update_box_01.hide()
 
         self.show()
 
@@ -49,6 +71,7 @@ class JobsWindow(QWidget):
         self.data_button.hide()
         self.back_button.show()
         self.welcome_label.hide()
+        self.update_box_01.show()
 
     def run_data_visualization(self):
         self.update_button.hide()
@@ -59,14 +82,28 @@ class JobsWindow(QWidget):
         self.back_button.move(25, 750)
         self.quit_button.move(715, 750)
         self.welcome_label.hide()
-        self.us_map.show()
+        self.map_visualization.show()
+        self.text_visualization_button.show()
 
     def go_back(self):
         self.back_button.hide()
         self.update_button.show()
         self.data_button.show()
         self.data_visualization_label.hide()
-        self.setGeometry(100, 100, 500, 500)
+        self.setGeometry(50, 50, 500, 500)
         self.back_button.move(25, 450)
         self.quit_button.move(415, 450)
         self.welcome_label.show()
+        self.text_data_visualization.hide()
+        print(self.update_box_01.text())
+        self.update_box_01.hide()
+        self.map_visualization.hide()
+        self.text_visualization_button.hide()
+
+    def text_visualization(self):
+        self.text_data_visualization.show()
+        self.text_data_visualization.setItem(0, 0, QTableWidgetItem("Hello World!"))
+
+    def run_map_visualization(self):
+        self.text_data_visualization.hide()
+        self.us_map.show()
